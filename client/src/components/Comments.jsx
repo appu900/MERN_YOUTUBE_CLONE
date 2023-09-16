2a88ae0d893b0431eca1ab21cf8e987634a0dd44
pabitra <89703515+appu900@users.noreply.github.com>
2023-09-16 21:37:45 +0530

    changes in UI

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,29 +1,45 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import chnlImage from "../images/girl-4448689_1280.jpg";
 
-const Container = styled.div`
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
   display: flex;
   align-items: center;
+  gap: 10px;
 `;
 
-const CommentsFromDb = styled.div`
-  display: flex;
-  align-items: center;
+const Avatar = styled.img`
+  width: 43px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid gray;
+  background-color: transparent;
+  outline: none;
+  width: 100%;
+  font-size: 15px;
+  padding: 5px;
 `;
 
 const Comments = () => {
-
-  const[comments,setComments] = useState([]);
-  const fetchComments = async() =>{
-    const response = await axios.get('')
-    setComments(response.data);
-  }  
-
-  
   return (
     <div>
-      <Container>hello</Container>
+      <Container>
+        <NewComment className="mt-4">
+          <Avatar src={chnlImage} />
+          <Input className="text-xm" placeholder="add a comment......" />
+        </NewComment>
+        <div>
+            
+        </div>
+      </Container>
     </div>
   );
 };
